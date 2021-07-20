7ff275d840af0ef373a85747d6a63b75bc046609
william-LK <84213291+william-LK@users.noreply.github.com>
2021-07-20 19:03:06 +0800

    Update index.js

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -8,26 +8,64 @@ const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+| ([^\
 const startTagClose = /^\s*(\/?)>/; // 匹配标签结束的  >
 const defaultTagRE = /\{\{((?:.|\n)+?)\}\}/g // {{}}
 
-// html字符串解释成dom树
+// 工厂函数 资料转对象
+function createAstElement(tagName, attrs) {
+    return {
+        tag: tagName, // 标签名
+        type: 1,
+        children: [],
+        parent: null,
+        attrs
+    }
+}
+
+
+// html字符串解释成dom树 将解析后的结果 组装成一个树结构 栈结构
+// 通过栈算法，把栈顶相同则弹出，不同则叠加的原则做处理
+let root = null;
+let stack = [];
 function start(tagName, attributes) {
-    console.log('start', tagName, attributes);
+    let parent = stack[stack.length - 1];
+    let element = createAstElement(tagName, attributes);
+    if (!root) {
+        root = element;
+    }
+    
+    element.parent = parent; // 当放入栈中时 记录父亲是谁
+    if (parent) {
+        parent.children.push(element);
+    }
+    stack.push(element);
 }
 
 function end(tagName) {
-    console.log('end', tagName);
+    let last = stack.pop();
+    if (last.tag !== tagName) {
+        throw new Error('标签有误')
+    }
 }
 
 function chars(text) {
     console.log('text', text);
+    text = text.replace(/\s/g, ""); // 删除空格
+    let parent = stack[stack.length - 1];
+    if (text) {
+        parent.children.push({
+            type: 3,
+            text
+        })
+    }
 }
 
+// 这里相当于编译器  
 function parserHTML(html) { // <div id="app">123123</div>
     // 前进 把前几位清空
     function advance(len) {
         html = html.substring(len);
     }
-    // 格式化开始标签
+    // 匹配开始的标签 <div>123</div> 中的 <div>,然后转化成对象
     function parseStartTag() {
+        // 匹配出前面的标签属性
         const start = html.match(startTagOpen);
         if (start) {
             const match = {
@@ -55,7 +93,7 @@ function parserHTML(html) { // <div id="app">123123</div>
     }
 
     while (html) { // 看要解析的内容是否存在。如果存在就不停地解析
-        let textEnd = html.indexOf('<');
+        let textEnd = html.indexOf('<'); // 当前解析的开头 <div>hello world</div>
         if (textEnd === 0) {
             const startTagMatch = parseStartTag(); // 解析开始标签
             // 处理开始标签
@@ -95,5 +133,5 @@ function parserHTML(html) { // <div id="app">123123</div>
 export function compileToFunction(template) {
 
     parserHTML(template);
-
+    console.log(root);
 }
